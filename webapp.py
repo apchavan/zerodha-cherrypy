e7ba2d8a2265623f206a004e4899ff2b228155c7
apchavan <ameyc96@gmail.com>
2019-06-14 19:33:45 +0530

    Route updated

--- webapp.py
+++ webapp.py
@@ -48,7 +48,7 @@ def index():
 	return render_template('index.html', redis_list_dict=redis_list_dict)
 
 
-@webapp.route("/", defaults={'search_name': ''})
+@webapp.route("/")
 @webapp.route("/<string:search_name>", methods=['POST', 'GET'])
 def search_button_clicked(search_name):
 	if request.method == 'POST':
@@ -59,8 +59,8 @@ def search_button_clicked(search_name):
 		return redirect(url_for('search_result', search_name=search_name))
 
 
-@webapp.route("/<search_name>", defaults={'search_name': 'TATA'})
-def search_result(search_name):
+@webapp.route("/<search_name>")
+def search_result(search_name=''):
 	# Create a client of redis.
 	# redis_client_obj = redis.StrictRedis(host=HOST_NAME, port=PORT_NO, db=DB_NO, charset='utf-8')
 	redis_client_obj = redis.from_url(url=os.environ['REDISCLOUD_URL'])
